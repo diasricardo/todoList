5495a294a40059e0c797c5502dcc8d44d2083a9a
Ricardo Dias <ricardo.dias@hotmail.com.br>
2025-02-26 16:25:41 -0300

    ddd

--- src/app.js
+++ src/app.js
@@ -18,7 +18,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Serve arquivos estáticos da pasta 'public'
+// app.use(express.static(path.join(__dirname, 'src', 'public')));
 app.use(express.static(path.join(__dirname, 'src', 'public')));
+
 
 // Define a pasta de views
 app.set("views", path.join(__dirname, "views"));
